bc0545308ecdf735542748c10a678a9812159d39
AndyEngland521 <andrew.england@sparkfun.com>
2019-05-15 11:59:38 -0600

    moving all the define statements and switching to static const char

--- SparkFunEnvironment.h
+++ SparkFunEnvironment.h
@@ -48,71 +48,7 @@ TODO:
 #define MODE_FORCED 0b01
 #define MODE_NORMAL 0b11
 
-//Register names:
-#define BME280_ADDRESS					0xEE
-#define BME280_DIG_T1_LSB_REG			0x88
-#define BME280_DIG_T1_MSB_REG			0x89
-#define BME280_DIG_T2_LSB_REG			0x8A
-#define BME280_DIG_T2_MSB_REG			0x8B
-#define BME280_DIG_T3_LSB_REG			0x8C
-#define BME280_DIG_T3_MSB_REG			0x8D
-#define BME280_DIG_P1_LSB_REG			0x8E
-#define BME280_DIG_P1_MSB_REG			0x8F
-#define BME280_DIG_P2_LSB_REG			0x90
-#define BME280_DIG_P2_MSB_REG			0x91
-#define BME280_DIG_P3_LSB_REG			0x92
-#define BME280_DIG_P3_MSB_REG			0x93
-#define BME280_DIG_P4_LSB_REG			0x94
-#define BME280_DIG_P4_MSB_REG			0x95
-#define BME280_DIG_P5_LSB_REG			0x96
-#define BME280_DIG_P5_MSB_REG			0x97
-#define BME280_DIG_P6_LSB_REG			0x98
-#define BME280_DIG_P6_MSB_REG			0x99
-#define BME280_DIG_P7_LSB_REG			0x9A
-#define BME280_DIG_P7_MSB_REG			0x9B
-#define BME280_DIG_P8_LSB_REG			0x9C
-#define BME280_DIG_P8_MSB_REG			0x9D
-#define BME280_DIG_P9_LSB_REG			0x9E
-#define BME280_DIG_P9_MSB_REG			0x9F
-#define BME280_DIG_H1_REG				0xA1
-#define BME280_CHIP_ID_REG				0xD0 //Chip ID
-#define BME280_RST_REG					0xE0 //Softreset Reg
-#define BME280_DIG_H2_LSB_REG			0xE1
-#define BME280_DIG_H2_MSB_REG			0xE2
-#define BME280_DIG_H3_REG				0xE3
-#define BME280_DIG_H4_MSB_REG			0xE4
-#define BME280_DIG_H4_LSB_REG			0xE5
-#define BME280_DIG_H5_MSB_REG			0xE6
-#define BME280_DIG_H6_REG				0xE7
-#define BME280_CTRL_HUMIDITY_REG		0xF2 //Ctrl Humidity Reg
-#define BME280_STAT_REG					0xF3 //Status Reg
-#define BME280_CTRL_MEAS_REG			0xF4 //Ctrl Measure Reg
-#define BME280_CONFIG_REG				0xF5 //Configuration Reg
-#define BME280_PRESSURE_MSB_REG			0xF7 //Pressure MSB
-#define BME280_PRESSURE_LSB_REG			0xF8 //Pressure LSB
-#define BME280_PRESSURE_XLSB_REG		0xF9 //Pressure XLSB
-#define BME280_TEMPERATURE_MSB_REG		0xFA //Temperature MSB
-#define BME280_TEMPERATURE_LSB_REG		0xFB //Temperature LSB
-#define BME280_TEMPERATURE_XLSB_REG		0xFC //Temperature XLSB
-#define BME280_HUMIDITY_MSB_REG			0xFD //Humidity MSB
-#define BME280_HUMIDITY_LSB_REG			0xFE //Humidity LSB
 
-#define CCS811_ADDRESS					0xB6
-#define CCS811_STATUS 0x00
-#define CCS811_MEAS_MODE 0x01
-#define CCS811_ALG_RESULT_DATA 0x02
-#define CCS811_RAW_DATA 0x03
-#define CCS811_ENV_DATA 0x05
-#define CCS811_NTC 0x06
-#define CCS811_THRESHOLDS 0x10
-#define CCS811_BASELINE 0x11
-#define CCS811_HW_ID 0x20
-#define CCS811_HW_VERSION 0x21
-#define CCS811_FW_BOOT_VERSION 0x23
-#define CCS811_FW_APP_VERSION 0x24
-#define CCS811_ERROR_ID 0xE0
-#define CCS811_APP_START 0xF4
-#define CCS811_SW_RESET 0xFF
 
 //Class SensorSettings.  This object is used to hold settings data.  The application
 //uses this classes' data directly.  The settings are adopted and sent to the sensor
