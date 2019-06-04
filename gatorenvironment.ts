c8683c432e3393f0ef31354cd1f048e44dc549fe
Andrew England <andrew.england@sparkfun.local>
2019-06-04 13:56:32 -0600

    forgot to change blockID

--- gatorenvironment.ts
+++ gatorenvironment.ts
@@ -31,7 +31,7 @@ namespace gatorEnvironment {
     // Functions for reading Particle from the gatorEnvironment in Particle or straight adv value
 	
 	//% weight=31 
-	//% blockId="gatorEnvironment_begin" 
+	//% blockId="gatorEnvironment_beginEnvironment" 
 	//% block="Initialize gator:Environment sensors"
 	//% shim=gatorEnvironment::beginEnvironment
 	export function beginEnvironment(){
