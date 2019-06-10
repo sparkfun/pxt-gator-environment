2526a92c70330b15af44891236a4f8ee02003d9d
Andrew England <andrew.england@sparkfun.local>
2019-06-10 13:37:42 -0600

    forgot to save test.ts

--- test.ts
+++ test.ts
@@ -17,38 +17,12 @@
  * Functions to operate the gatorEnvironment sensor
  */
 
- enum measurementType{
-	 degreesC=1,
-	 degreesF=2,
-	 humidity=3,
-	 pressure=4,
-	 eCO2=5,
-	 TVOC=6,
- } 
- 
-//% color=#f44242 icon="\uf0c2"
-namespace gatorEnvironment {
-    // Functions for reading Particle from the gatorEnvironment in Particle or straight adv value
-	
-	/**
-	* Initialize the gator:environment sensor for readings
-	*/
-	//% weight=32 
-	//% blockId="gatorEnvironment_beginEnvironment" 
-	//% block="initialize gator:Environment sensors"
-	//% shim=gatorEnvironment::beginEnvironment
-	export function beginEnvironment(){
-		return
-	}
-		
-	/**
-	* Grab the temperature, humidity, pressure, equivalent C02, or total Volatile Organic Compounds from the gator:environment
-	*/
-	//% weight=31
-	//% blockId="gatorEnvironment_getMeasurement"
-	//% block="get %measurementType | value"
-	//% shim=gatorEnvironment::getMeasurement
-	export function getMeasurement(type: measurementType): number{
-		return 0
-	}
-}
+gatorEnvironment.beginEnvironment()
+basic.forever(function () {
+    serial.writeLine("" + gatorEnvironment.getMeasurement(measurementType.degreesC))
+    serial.writeLine("" + gatorEnvironment.getMeasurement(measurementType.degreesF))
+    serial.writeLine("" + gatorEnvironment.getMeasurement(measurementType.humidity))
+    serial.writeLine("" + gatorEnvironment.getMeasurement(measurementType.pressure))
+    serial.writeLine("" + gatorEnvironment.getMeasurement(measurementType.eCO2))
+    serial.writeLine("" + gatorEnvironment.getMeasurement(measurementType.TVOC))
+})
